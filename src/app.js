3464d5bbe1c68c0521e3b29689d3414ac18b8525
Prasanna_bala <bprasanna83@gmail.com>
2022-04-20 17:28:46 +0200

    pm2

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ msg: 'Hello!' });
 });
 
-app.get('/api/hello', (req, res) => {
+app.get('/hello', (req, res) => {
   res.json({ msg: 'Hello!', status: 200 });
 });
 
-app.get('/api/dashboard', (req, res) => {
+app.get('/dashboard', (req, res) => {
   res.json({ msg: 'Hello!', status: 201 });
 });
 
